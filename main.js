550181a099e3270ad6906627b6afbed158ec8a2b
Fleming <limin.fei@amd.com>
2022-11-29 02:24:09 +0800

    update

--- main.js
+++ main.js
@@ -3,27 +3,32 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const electron = require('electron')
+const menu = electron.Menu
 
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 900,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  // 加载 index.html
+  // hide menu bar
+  menu.setApplicationMenu(null)
+
+  // load index.html
   mainWindow.loadFile('index.html')
 
-  // 打开开发工具
-  // mainWindow.webContents.openDevTools()
+  // open chrome dev tools like F12
+  mainWindow.webContents.openDevTools()
 }
 
-// 这段程序将会在 Electron 结束初始化
-// 和创建浏览器窗口的时候调用
-// 部分 API 在 ready 事件触发后才能使用。
+// Code under will init after createWindow
+// Also call when the browser window is created
+// Some APIs are not available until ready()
 app.whenReady().then(() => {
   createWindow()
 
@@ -34,7 +39,7 @@ app.whenReady().then(() => {
   })
 })
 
-// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 There, it's common
+// Quit the program when all windows are closed except for macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
@@ -42,4 +47,4 @@ app.on('window-all-closed', () => {
 })
 
 // In this file you can include the rest of your app's specific main process
-// code. 也可以拆分成几个文件，然后用 require 导入。
+// code. You can also split it into several files and import them with require.
--- preload.js
+++ preload.js
@@ -1,7 +1,6 @@
 // preload.js
 
 // All the Node.js APIs are available in the preload process.
-// 它拥有与Chrome扩展一样的沙盒。
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
