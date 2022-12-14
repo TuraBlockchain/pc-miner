7bb7e72fd6b7316717470fd8d29016bd908f58c7
Fleming <limin.fei@amd.com>
2022-12-15 02:26:02 +0800

    use vuetify 3.0.0 <v-layout>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-app>
+  <v-layout>
     <PCMRoot/>
-  </v-app>
+  </v-layout>
 </template>
 
 <script>
--- src/components/PCMRoot.vue
+++ src/components/PCMRoot.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <v-navigation-drawer permanent>
+      <v-list nav>
+        <v-list-item v-for="item in sidebarItems" :prepend-icon="item.icon" :title="item.value" :value="item.value" :key="item.value" @click="itemOnClick(item.value)"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar>{{ appBarTitle }}</v-app-bar>
+    <v-main>
+      <v-card height="200px"></v-card>
+    </v-main>
   </div>
 </template>
 <style scoped>
@@ -13,10 +22,21 @@ export default {
   },
   data() {
     return {
+      appBarTitle: "",
+      sidebarItems: [
+        { icon: 'mdi-home', value: 'Home'},
+        { icon: 'mdi-pickaxe', value: 'Minning'},
+        { icon: 'mdi-swap-horizontal', value: 'Transactions'},
+        { icon: 'mdi-chart-bell-curve', value: 'Plotting'},
+        { icon: 'mdi-view-list', value: 'Commitment'},
+        { icon: 'mdi-cog-outline', value: 'Settings'},
+      ],
     };
   },
   methods: {
-
+    itemOnClick(value) {
+      this.appBarTitle = value;
+    }
   },
   mounted: function() {
   }
